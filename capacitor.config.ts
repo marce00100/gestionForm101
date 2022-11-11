49191c99e72f1ca6b5afddb69d5c9f5aa1de7207
marce <marce00100@gmail.com>
2022-11-10 20:20:30 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontapp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
